6ddf398fb4d8370035adc6fbc12495e683a121d3
Bnorbis <brunanaian1@gmail.com>
2025-09-24 00:03:09 -0300

    chore: initial project setup - adding header

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,22 @@
 import Banner from "../Banner/Banner.jsx";
 import Footer from "../Footer/Footer.jsx";
+import styles from "./Home.module.css";
+import {Link} from "react-router-dom";
 
 function Home() {
 
     return (
         <div className="homeMain">
+            <div className={styles.header}>
+                <Link
+                    to="/"
+                    onClick={() => {
+                    }}
+                    className={styles.logo}
+                >
+                    Pet Care
+                </Link>
+            </div>
             <Banner/>
             <Footer/>
         </div>
