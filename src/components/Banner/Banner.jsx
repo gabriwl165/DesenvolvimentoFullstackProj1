41796b16c331e94b0f3cdeb7a037472f3fff9964
Bnorbis <brunanaian1@gmail.com>
2025-09-24 20:22:45 -0300

    fix: text from banner do not overlap

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,8 +5,11 @@ function Banner() {
     return (
         <div className={styles.container}>
             <img src="/DogBanner.png" alt="DogBanner"/>
-            <h2 className={styles.mainText}>Animal breeds were made to be predictable!</h2>
-            <p className={styles.secondText}>Discover everything about dog or cat breeds and how to care for them.</p>
+            <div className={styles.bannerText}>
+                <h2 className={styles.mainText}>Animal breeds were made to be predictable!</h2>
+                <p className={styles.secondText}>Discover everything about dog or cat breeds and how to care for
+                    them.</p>
+            </div>
         </div>
     )
 }
